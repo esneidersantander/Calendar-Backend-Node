a7fba38ffebffa010378fe4e36fe681d5855b9a1
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-03-03 21:34:24 -0500

    cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require ('express');
 require('dotenv').config();
-const {dbConnection} = require('./database/config')
+const {dbConnection} = require('./database/config');
+const cors = require ('cors');
+
+
 //crear el servidor de express
 const app = express();
 
 //base de datos
 dbConnection();
+
+//cors
+app.use(cors());
 
 //directorio publico
 app.use( express.static('public') );
